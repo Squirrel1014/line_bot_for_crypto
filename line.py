42f178941abcb61ffb609e28705d33d5816b79d3
Squirrel1014 <154125024+Squirrel1014@users.noreply.github.com>
2025-09-12 03:53:39 +0800

    Update spot.py

--- line.py
+++ line.py
@@ -1,64 +1,49 @@
 import os
 import sys
-import json , ssl
+import json
 import requests
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from decimal import Decimal, getcontext
 
-api_key = os.getenv('coinmarket_api_key' , None)
-if api_key is None :
-    print('尚未設置api金鑰')
-    exit(1)
+#api_key放在本地terminal
+bsc_api_key = os.environ.get('BINANCE_API_KEY')
+secret_key = os.environ.get('BINANCE_SECRET_KEY')
+if not bsc_api_key or not secret_key :
+    print('沒找到金鑰')
+    sys.exit()
 
-def get_tokenvalue(symbol) :
-    domain_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-    parameters = {
-        'symbols' : symbol ,
-        'convert' : 'USD'
-    }
-    headers = {
-    'Accept': 'application/json',
-    'Accept-Encoding': 'deflate, gzip'
-    }
+#api 程式碼
+def get_token_price(symbol) :
+    bsc_url = 'https://data-api.binance.vision/api/v3'
+    endpoint = '/ticker/price'
+    parameters = {'symbol' : symbol.upper()}
     try :
-        response = requests.get(domain_url , param = parameters , headers = headers)
+        response = requests.get(f"{bsc_url}{endpoint}", params=parameters, timeout=10)
         response.raise_for_status()
-        token_price = response.json()
-        if price['status']['error_code'] == 0 :
-            crypto_data = token_price['data'].get(symbol.upper())
-            if crypto_data :
-                price = crypto_data['quote']['USD']['price']
-                return {
-                    "symbol" : 'crypto_data'['symbol'] ,
-                    "name" : 'crypto_data'['name'] ,
-                    "price" : round(price , 3)
-                }
-            else :
-                print('找不到{symbol}的資料')
-                return None
-        else:
-            print(f"API 請求失敗：{price['status']['error_message']}")
-            return None
-    except (ConnectionError, Timeout, TooManyRedirects) as e:
-        print(f"網路連線出事啦：{e}")
-        return None
+        data = response.json()
+        price = data.get('price')
+        return price
     except requests.exceptions.HTTPError as e:
-        print(f"HTTP 請求有問題哦：{e}")
+        print(f"HTTP 錯誤發生： {e}")
         return None
-    except Exception as e:
-        print(f"不知道怎麼了：{e}")
+    except TooManyRedirects as e :
+        print(f"請求次數過多： {e}")
         return None
-#測試區
+    except requests.exceptions.RequestException as e:
+        print(f"請求發生錯誤：{e}")
+        return None
+
 if __name__ == '__main__':
-    btc_data = get_tokenvalue('BTC')
-    if btc_data:
-        print("成功取得 Bitcoin 價格：")
-        print(json.dumps(btc_data, indent=4))
-    else:
-        print("無法取得 Bitcoin 價格。")
-
-    print("\n---")
-
-
-#目前進度：line官方帳號設定完成，前端尚未設置完成
-#要找雲端伺服器部署
-#coinmarket api 部署環境變數
+    while True :
+        token_name = input('輸入想要知道的幣種名稱，如btcusdt(必須是一個交易對哦)')
+        if token_name.strip().lower() == 'exit':
+            print("程式結束。")
+            break
+        price = get_token_price(token_name)
+        if price :
+            quantizer = Decimal('0.0001')
+            price_decimal = Decimal(price)
+            show_price = price_decimal.quantize(quantizer)
+            print(f"{token_name}現在價格是：{show_price}")
+        else :
+            print('f"無法取得{token_name}的價格"')
